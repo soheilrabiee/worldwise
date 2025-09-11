d05d37c701fd058ee16f62f15fb1cd20c9317110
Soheil <iamsoheil.r@gmail.com>
2025-09-11 21:58:54 +0330

    Add cities list items

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,10 +1,15 @@
 import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
+import Message from "./Message";
 
 function CityList({ cities, isLoading }) {
-    console.log(cities);
     if (isLoading) return <Spinner />;
+
+    if (!cities.length)
+        return (
+            <Message message="Add your first city by clicking a city on the map!" />
+        );
 
     return (
         <ul className={styles.cityList}>
