98ede76cb0ffa221091f23bc88847d4e941e14a6
Soheil <iamsoheil.r@gmail.com>
2025-09-13 16:58:35 +0330

    Add button component

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -67,7 +68,7 @@ function App() {
                             />
                         }
                     />
-                    <Route path="form" element={<p>Form</p>} />
+                    <Route path="form" element={<Form />} />
                 </Route>
                 {/* Anything that is not a valid path */}
                 <Route path="*" element={<PageNotFound />} />
