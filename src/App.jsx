00bacca51fc4127fb71d7878625644ddcae3a8bd
Soheil <iamsoheil.r@gmail.com>
2025-09-13 17:11:26 +0330

    Use navigation component as redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -42,12 +42,7 @@ function App() {
                 <Route path="login" element={<Login />} />
                 <Route path="app" element={<AppLayout />}>
                     {/* The root path of the parent */}
-                    <Route
-                        index
-                        element={
-                            <CityList cities={cities} isLoading={isLoading} />
-                        }
-                    />
+                    <Route index element={<Navigate replace to="cities" />} />
 
                     <Route
                         path="cities"
