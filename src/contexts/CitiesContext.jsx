27f46cd21f20b6c42556ffebc3ca50a56c54a3d1
Soheil <iamsoheil.r@gmail.com>
2025-09-16 01:13:53 +0330

    Handle the deletion of a city

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -52,7 +52,22 @@ function CitiesProvider({ children }) {
 
             setCities((cities) => [...cities, data]);
         } catch {
-            alert("There was an error loading the data for the city ...");
+            alert("There was an error creating the city!");
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    async function deleteCity(id) {
+        try {
+            setIsLoading(true);
+            await fetch(`${BASE_URL}/cities/${id}`, {
+                method: "DELETE",
+            });
+
+            setCities((cities) => cities.filter((city) => city.id !== id));
+        } catch {
+            alert("There was an error deleting the city!");
         } finally {
             setIsLoading(false);
         }
@@ -60,7 +75,14 @@ function CitiesProvider({ children }) {
 
     return (
         <CitiesContext.Provider
-            value={{ cities, isLoading, currentCity, getCity, createCity }}
+            value={{
+                cities,
+                isLoading,
+                currentCity,
+                getCity,
+                createCity,
+                deleteCity,
+            }}
         >
             {children}
         </CitiesContext.Provider>
