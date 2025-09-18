494c790eabe3990346c575b450be78a7b6ea7bba
Soheil <iamsoheil.r@gmail.com>
2025-09-18 20:22:23 +0330

    Add lazy loading to the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,70 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import Homepage from "./pages/Homepage";
-import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./pages/AppLayout";
-import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-import { CitiesProvider } from "./contexts/CitiesContext";
-import { AuthProvider } from "./contexts/FakeAuthContext";
-import ProtectedRoute from "./pages/ProtectedRoute";
+import SpinnerFullPage from "./components/SpinnerFullPage";
+
+// import Product from "./pages/Product";
+// import Pricing from "./pages/Pricing";
+// import Homepage from "./pages/Homepage";
+// import PageNotFound from "./pages/PageNotFound";
+// import AppLayout from "./pages/AppLayout";
+// import Login from "./pages/Login";
+
+// Add lazy loading
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Login = lazy(() => import("./pages/Login"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 
 function App() {
     return (
         <CitiesProvider>
             <AuthProvider>
                 <BrowserRouter>
-                    <Routes>
-                        <Route index element={<Homepage />} />
-                        <Route path="product" element={<Product />} />
-                        <Route path="pricing" element={<Pricing />} />
-                        <Route path="login" element={<Login />} />
-                        <Route
-                            path="app"
-                            element={
-                                <ProtectedRoute>
-                                    <AppLayout />
-                                </ProtectedRoute>
-                            }
-                        >
-                            {/* The root path of the parent */}
+                    <Suspense fallback={<SpinnerFullPage />}>
+                        <Routes>
+                            <Route index element={<Homepage />} />
+                            <Route path="product" element={<Product />} />
+                            <Route path="pricing" element={<Pricing />} />
+                            <Route path="login" element={<Login />} />
                             <Route
-                                index
-                                element={<Navigate replace to="cities" />}
-                            />
+                                path="app"
+                                element={
+                                    <ProtectedRoute>
+                                        <AppLayout />
+                                    </ProtectedRoute>
+                                }
+                            >
+                                {/* The root path of the parent */}
+                                <Route
+                                    index
+                                    element={<Navigate replace to="cities" />}
+                                />
 
-                            <Route path="cities" element={<CityList />} />
+                                <Route path="cities" element={<CityList />} />
 
-                            {/* Pass params in the URL */}
-                            <Route path="cities/:id" element={<City />} />
+                                {/* Pass params in the URL */}
+                                <Route path="cities/:id" element={<City />} />
 
-                            <Route path="countries" element={<CountryList />} />
-                            <Route path="form" element={<Form />} />
-                        </Route>
-                        {/* Anything that is not a valid path */}
-                        <Route path="*" element={<PageNotFound />} />
-                    </Routes>
+                                <Route
+                                    path="countries"
+                                    element={<CountryList />}
+                                />
+                                <Route path="form" element={<Form />} />
+                            </Route>
+                            {/* Anything that is not a valid path */}
+                            <Route path="*" element={<PageNotFound />} />
+                        </Routes>
+                    </Suspense>
                 </BrowserRouter>
             </AuthProvider>
         </CitiesProvider>
